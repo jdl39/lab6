ca324cff455a56c90b9b253c676b579a613aa413
Jimmy Lee <jdl39@stanford.edu>
2014-02-12 23:18:40 +0000

    finished lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	console.log("Getting " + "/project/" + idNumber);
+
+	$.get("/project/" + idNumber, function(result) {
+		console.log(result);
+
+		var newHTML = ''
+		newHTML += '<img src="' + result["image"] + '" class="detailsImg" />'
+		newHTML += '<h3>' + result["date"] + '</h3>'
+		newHTML += result["summary"]
+
+		$("#" + projectID + " .details").html(newHTML);
+	});
 }
 
 /*
@@ -35,4 +47,13 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", function(result) {
+		var colors = result["colors"]["hex"]
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
